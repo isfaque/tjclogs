876dc00d4c04370f30a33fc4cf704a86841489d0
isfaqueneosoft <md.isfaque@neosoftmail.com>
2022-11-28 19:35:26 +0530

    updated port

--- api/config/config.js
+++ api/config/config.js
@@ -25,8 +25,8 @@ const config = {
     },
     prod: {
         host: 'middleware-log.tjc.co.uk:',
-        port: 443,
-        baseUrl: 'http://middleware-log.tjc.co.uk:443/',
+        port: 3000,
+        baseUrl: 'http://middleware-log.tjc.co.uk:3000/',
         DATABASE: {
             dbname: 'tjclogs',
             host: 'mongodb://127.0.0.1:',
